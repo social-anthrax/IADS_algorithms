ebee1501d965fc1ef9e66fd8d407187f95e97e03
Artemis Livingstone <ar.d.livingstone@gmail.com>
2022-02-07 19:36:45 +0000

    Implemented Floyd-Warshall

---
+++ graph_algos/FloydWarshall.py
@@ -0,0 +1,90 @@
+from asyncore import loop
+import math
+from random import randrange
+
+
+class GraphAdjacencyMatrix:
+    def __init__(self, vertices, edges) -> None:
+        self.vertices = vertices
+        self.edges = edges
+
+
+def floyd_warshall(
+    G: GraphAdjacencyMatrix,
+) -> tuple[list[list[int]], list[list[int]]]:
+    # this assumes that the unreachable sections are initialised to math.inf
+    d_curr = G.edges
+
+    d_next = d_curr
+
+    # Initialise each previous node to -1
+    pi_curr = [
+        [-1 for _ in range(0, len(G.vertices))] for _ in range(0, len(G.vertices))
+    ]
+
+    # Set up distance from each node to itself to 0.
+    # Set up the previous node to itself to itself.
+    for i in range(0, len(G.vertices)):
+        d_curr[i][i] = 0
+        pi_curr[i][i] = i
+
+    # For each edge, initialise the previously found edge
+    for i in range(len(G.vertices)):
+        for j in range(len(G.vertices)):
+            if G.edges[i][j] != math.inf and G.edges[i][j] != 0:
+                pi_curr[i][j] = i
+
+    # Copy pi_curr
+    pi_next = pi_curr
+
+    # as seen in lectures.
+    for k in range(0, len(G.vertices)):
+        for i in range(0, len(G.vertices)):
+            for j in range(0, len(G.vertices)):
+                d_next[i][j] = d_curr[i][j]
+                pi_next[i][j] = pi_curr[i][j]
+                if j != i and (d_curr[i][k] + d_curr[k][j]) < d_next[i][j]:
+                    d_next[i][j] = d_curr[i][k] + d_curr[k][j]
+                    pi_next[i][j] = pi_curr[k][j]
+
+    return (d_next, pi_next)
+
+
+def reconstruct_path(pi: list[list[int]], start: int, end: int) -> list[int]:
+    """
+    Given the pi matrix returns the **Indices** for the vertices in the graph.
+    You may need to loop through the graph's indices to get the values for path.
+    """
+
+    if pi[start][end] == -1:
+        return []
+
+    path = [end]
+    while start != end:
+        end = pi[start][end]
+        path.append(end)
+
+    return path[::-1]
+
+
+# Example.
+if __name__ == "__main__":
+    weights = [
+        [0, math.inf, 6, 3, math.inf],
+        [3, 0, math.inf, math.inf, math.inf],
+        [math.inf, math.inf, 0, 2, math.inf],
+        [math.inf, 1, 1, 0, math.inf],
+        [math.inf, 4, math.inf, 2, 0],
+    ]
+
+    vertices = [1, 2, 3, 4, 5]
+    graph = GraphAdjacencyMatrix(vertices, weights)
+    path = []
+
+    (d, pi) = floyd_warshall(graph)
+
+    # reconstruct to print the correct vertices
+    # we use 4 and 1 when referring to 5 and 2, as the vertices are 1 indexed
+    for x in reconstruct_path(pi, 4, 1):
+        path.append(graph.vertices[x])
+    print(path)
