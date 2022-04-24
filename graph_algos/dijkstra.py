704954c2fc4d6fec098b52f27cbae6e201ed39dc
Bence Szilagyi <szilagyi.bence00@gmail.com>
2022-04-24 18:17:52 +0100

    Dijkstra with pi array

--- graph_algos/dijkstra.py
+++ graph_algos/dijkstra.py
@@ -14,6 +14,7 @@ class Graph:
 
     def dijkstra(self, start_vertex):
         D = {v:float('inf') for v in range(self.v)}
+        pi = {v:None for v in range(self.v)}
         D[start_vertex] = 0
     
         pq = PriorityQueue()
@@ -22,7 +23,7 @@ class Graph:
         while not pq.empty():
             (dist, current_vertex) = pq.get()
             self.visited.append(current_vertex)
-    
+
             for neighbor in range(self.v):
                 if self.edges[current_vertex][neighbor] != -1:
                     distance = self.edges[current_vertex][neighbor]
@@ -30,15 +31,22 @@ class Graph:
                         old_cost = D[neighbor]
                         new_cost = D[current_vertex] + distance
                         if new_cost < old_cost:
+                            pi[neighbor] = current_vertex
                             pq.put((new_cost, neighbor))
                             D[neighbor] = new_cost
         return D
 
     def dijkstra_traced(self, start_vertex):
         print("Start:")
+
+        # InitializeSingleSource
         D = {v:float('inf') for v in range(self.v)}
+        pi = {v:None for v in range(self.v)}
         D[start_vertex] = 0
+        #
+
         print("D", D)
+        print("pi", pi)
     
         pq = PriorityQueue()
         pq.put((0, start_vertex))
@@ -48,17 +56,26 @@ class Graph:
             (dist, current_vertex) = pq.get()
             self.visited.append(current_vertex)
     
+            # for x in Out(u) -> x == v  == neighbor
             for neighbor in range(self.v):
+                # u == current_vertex
                 if self.edges[current_vertex][neighbor] != -1:
+                #
+                    # Relax
                     distance = self.edges[current_vertex][neighbor]
                     if neighbor not in self.visited:
                         old_cost = D[neighbor]
+                        if old_cost == float('inf'): print("inf at", neighbor)
                         new_cost = D[current_vertex] + distance
                         if new_cost < old_cost:
+                            # Q.insertItem(d[v ], v) or Q.reduceKey(d[v], v)
+                            pi[neighbor] = current_vertex
                             pq.put((new_cost, neighbor))
                             D[neighbor] = new_cost
+                    #
                 print("step")
                 print("D", D)
+                print("pi", pi)
                 print("priority queue", pq.queue)
         return D
 
